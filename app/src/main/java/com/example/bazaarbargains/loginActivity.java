34f65a66370cb36c12e689e1edeedefdaaa2ddf7
HazeemHussain <hhzmuk@gmail.com>
2023-04-29 15:50:05 +1200

    Adding text password features

--- app/src/main/java/com/example/bazaarbargains/loginActivity.java
+++ app/src/main/java/com/example/bazaarbargains/loginActivity.java
@@ -35,33 +35,46 @@ public class loginActivity extends AppCompatActivity {
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                verifyingLogin();
+                checkingLoginFields();
             }
         });
 
         forgotPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(loginActivity.this, ForgotPasswordActivity.class );
+                Intent intent = new Intent(loginActivity.this, ForgotPasswordActivity.class);
                 startActivity(intent);
             }
         });
 
-
-
     }
 
-    private void verifyingLogin() {
+    private void checkingLoginFields() {
         String userName = inputUserName.getText().toString();
         String password = inputPassword.getText().toString();
 
-        if (userName.isEmpty() || password.isEmpty()) {
-            Toast.makeText(loginActivity.this, "PLEASE ENTER YOUR DETAILS", Toast.LENGTH_SHORT).show();
+        if (userName.isEmpty()) {
+            Toast.makeText(loginActivity.this, "PLEASE ENTER YOUR USERNAME", Toast.LENGTH_SHORT).show();
 
+        } else if (password.isEmpty()) {
+            Toast.makeText(loginActivity.this, "PLEASE ENTER YOUR PASSWORD", Toast.LENGTH_SHORT).show();
         } else {
 
+            loadingbar.setTitle("LOGIN ACCOUNT");
+            loadingbar.setMessage("VERIFYING YOUR DETAILS, PLEASE BE PATIENT");
+            loadingbar.setCanceledOnTouchOutside(false);
+            loadingbar.show();
 
+
+            verifyingLogin(userName, password);
         }
 
     }
+
+    private void verifyingLogin(String userName, String password) {
+
+        
+    }
+
+
 }
